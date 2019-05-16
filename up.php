9a81c9deb27f2eab138f604da76499b720540696
ANAS <47825398+4n45@users.noreply.github.com>
2019-05-16 15:59:27 +0430

    Update up.php

--- up.php
+++ up.php
@@ -4,7 +4,7 @@
   <title>Upload your files</title>
 </head>
 <body>
-  <form enctype="multipart/form-data" action="upload.php" method="POST">
+  <form enctype="multipart/form-data" action="" method="POST">
     <p>Upload your file</p>
     <input type="file" name="uploaded_file"></input><br />
     <input type="submit" value="Upload"></input>
@@ -14,7 +14,7 @@
 <?PHP
   if(!empty($_FILES['uploaded_file']))
   {
-    $path = "uploads/";
+    $path = "./";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
       echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
